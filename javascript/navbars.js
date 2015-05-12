e7835c03cf6030c1d3b8c080dd5738c698ae18d2
A13xemz <a13xemz@gmail.com>
2015-05-11 17:38:55 -0700

    may 11

--- javascript/navbars.js
+++ javascript/navbars.js
@@ -14,3 +14,20 @@ $(document).scroll(function() {
     $("navbarmain").fadeOut();
   }
 });
+function clearCurrentLink(){
+    var a = document.getElementsByTagName("A");
+    for(var i=0;i<a.length;i++){
+        if(a[i].href.split("#")[0] == window.location.href.split("#")[0]){
+            removeNode(a[i]);
+        }
+    }
+}
+
+function removeNode(n){
+    if(n.hasChildNodes()){
+        for(var i=0;i<n.childNodes.length;i++){
+            n.parentNode.insertBefore(n.childNodes[i].cloneNode(true),n);
+        }
+    }
+    n.parentNode.removeChild(n);
+}
