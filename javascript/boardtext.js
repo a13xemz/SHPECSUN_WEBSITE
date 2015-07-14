ef9aca4fd09864a5fbfb9cbc7dcd252a14a5a9bd
A13xemz <a13xemz@gmail.com>
2015-07-13 20:03:41 -0700

    Couple more edits

--- javascript/boardtext.js
+++ javascript/boardtext.js
@@ -3,46 +3,11 @@ $(document).ready(function() {
             url : "helloworld.txt",
             dataType: "text",
             success : function (data) {
-                $("treasurytext").html(data);
+                $("profilequote").html(data);
             }
         });
     });
-$(document).ready(function() {
-        $.ajax({
-            url : "helloworld.txt",
-            dataType: "text",
-            success : function (data) {
-                $("socialtext").html(data);
-            }
-        });
-    });
-$(document).ready(function() {
-        $.ajax({
-            url : "helloworld.txt",
-            dataType: "text",
-            success : function (data) {
-                $("communicationstext").html(data);
-            }
-        });
-    });
-$(document).ready(function() {
-        $.ajax({
-            url : "helloworld.txt",
-            dataType: "text",
-            success : function (data) {
-                $("presidenttext").html(data);
-            }
-        });
-    });
-$(document).ready(function() {
-        $.ajax({
-            url : "helloworld.txt",
-            dataType: "text",
-            success : function (data) {
-                $("vptext").html(data);
-            }
-        });
-    });
+
 $(document).ready(function() {
             $("hidebutton").click(function() {
             $("row2").slideToggle("slow");
--- javascript/navbars.js
+++ javascript/navbars.js
@@ -1,7 +1,7 @@
+
 $(document).ready( function() {
         $('.navbar-inverse').delay(2600).fadeIn();
-      });
-$(document).ready( function() {
         $('#maincaption img').delay(2500).fadeOut();
         $('#maincaption h2').delay(2500).fadeIn();
+        $('#maincaption h2').delay(5000).fadeOut();
       });
