74915bf4ddf7cd29f321d839a6067eeee8154137
A13xemz <a13xemz@gmail.com>
2015-07-20 09:52:06 -0700

    Spring Cleaning in Summer

--- javascript/letters.js
+++ javascript/letters.js
@@ -1,38 +1,41 @@
-
 $(document).ready(function() {
     $("letters").hover(function() {
-    $("letters img").css("padding-right","0px")
-    $("stext").stop(true, false).show("slow").fadeIn("slow");
-},function(){
-        $("letters").css("padding-right","20px");
-        $("stext").stop(true, false).hide("slow").fadeOut("slow");}
-                      )});
+        $("letters img").css("padding-right", "0px")
+        $("stext").stop(true, false).show("slow").fadeIn("slow");
+    }, function() {
+        $("letters").css("padding-right", "20px");
+        $("stext").stop(true, false).hide("slow").fadeOut("slow");
+    })
+});
 
 $(document).ready(function() {
     $("letterh").hover(function() {
-     $("letterh img").css("padding-right","0px");
-    $("htext").stop(true, false).show("slow").fadeIn("slow");
-},function(){
-        $("letterh").css("padding-right","20px");
-        $("htext").stop(true, false).hide("slow").fadeOut("slow");}
-                      )});
+        $("letterh img").css("padding-right", "0px");
+        $("htext").stop(true, false).show("slow").fadeIn("slow");
+    }, function() {
+        $("letterh").css("padding-right", "20px");
+        $("htext").stop(true, false).hide("slow").fadeOut("slow");
+    })
+});
 
 $(document).ready(function() {
     $("letterp").hover(function() {
-     $("letterp img").css("padding-right","0px");
-    $("ptext").stop(true, false).show("slow").fadeIn("slow");
-},function(){
-        $("letterp").css("padding-right","20px");
-        $("ptext").stop(true, false).hide("slow").fadeOut("slow");}
-                      )});
+        $("letterp img").css("padding-right", "0px");
+        $("ptext").stop(true, false).show("slow").fadeIn("slow");
+    }, function() {
+        $("letterp").css("padding-right", "20px");
+        $("ptext").stop(true, false).hide("slow").fadeOut("slow");
+    })
+});
 $(document).ready(function() {
     $("lettere").hover(function() {
-     $("lettere img").css("padding-right","0px");
-    $("etext").stop(true, false).show("slow").fadeIn("slow");
-},function(){
-        $("lettere").css("padding-right","20px");
-        $("etext").stop(true, false).hide("slow").fadeOut("slow");}
-                      )});
+        $("lettere img").css("padding-right", "0px");
+        $("etext").stop(true, false).show("slow").fadeIn("slow");
+    }, function() {
+        $("lettere").css("padding-right", "20px");
+        $("etext").stop(true, false).hide("slow").fadeOut("slow");
+    })
+});
 $('down').click(function() {
     $('html, body').animate({
         scrollTop: $('footer').offset().top
